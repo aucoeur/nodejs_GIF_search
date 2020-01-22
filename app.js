b306b5e611401172be476a8325511d1b474befc8
aucoeur <aucoeur.ngo@students.makeschool.com>
2020-01-22 15:32:55 -0800

    Add middleware

--- app.js
+++ app.js
@@ -5,11 +5,27 @@ const express = require('express');
 const app = express();
 
 // Middleware
+const exphbs  = require('express-handlebars');
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 
 // Routes
 app.get('/', (req, res) => {
-  res.send('Hello Squirrel');
-});
+  // set the url of the gif
+  const gifUrl = 'https://media1.tenor.com/images/c1eee7009b69805d1b407215de01c85a/tenor.gif?itemid=15538987'
+  // render the hello-gif view, passing the gifUrl into the view to be displayed
+  res.render('hello-gif', { gifUrl })
+})
+
+app.get('/greetings/:name', (req, res) => {
+  // grab the name from the path provided
+  const name = req.params.name;
+  // render the greetings view, passing along the name
+  res.render('greetings', { name });
+})
+
+
 // Start Server
 
 app.listen(3000, () => {
