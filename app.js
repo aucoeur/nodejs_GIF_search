a0bf7325566d1ba53e32c9e41947b677d469e785
aucoeur <aucoeur.ngo@students.makeschool.com>
2020-01-22 15:47:37 -0800

    Add search form

--- app.js
+++ app.js
@@ -11,11 +11,18 @@ app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
 // Routes
+// app.get('/', (req, res) => {
+//   // set the url of the gif
+//   const gifUrl = 'https://media1.tenor.com/images/c1eee7009b69805d1b407215de01c85a/tenor.gif?itemid=15538987'
+//   // render the hello-gif view, passing the gifUrl into the view to be displayed
+//   res.render('hello-gif', { gifUrl })
+// })
+
+// example URL "http://localhost:3000/?term=hey"
 app.get('/', (req, res) => {
-  // set the url of the gif
-  const gifUrl = 'https://media1.tenor.com/images/c1eee7009b69805d1b407215de01c85a/tenor.gif?itemid=15538987'
-  // render the hello-gif view, passing the gifUrl into the view to be displayed
-  res.render('hello-gif', { gifUrl })
+  console.log(req.query) // => "{ term: hey" }/
+
+  res.render('home')
 })
 
 app.get('/greetings/:name', (req, res) => {
