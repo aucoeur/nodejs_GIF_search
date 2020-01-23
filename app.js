2cba62fa8c1cb933627ac4812ae1d67206e7e49c
aucoeur <aucoeur.ngo@students.makeschool.com>
2020-01-22 16:51:33 -0800

    styled page

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
+require('dotenv').config();
+
 // Require Libraries
 const express = require('express');
 
+const Tenor = require("tenorjs").client({
+    // Replace with your own key
+    "Key": process.env.TENOR_API_KEY, // https://tenor.com/developer/keyregistration
+    "Filter": "high", // "off", "low", "medium", "high", not case sensitive
+    "Locale": "en_US", // Your locale here, case-sensitivity depends on input
+});
+
 // App Setup
 const app = express();
+app.use(express.static('public'));
 
 // Middleware
 const exphbs  = require('express-handlebars');
@@ -18,11 +28,20 @@ app.set('view engine', 'handlebars');
 //   res.render('hello-gif', { gifUrl })
 // })
 
-// example URL "http://localhost:3000/?term=hey"
 app.get('/', (req, res) => {
-  console.log(req.query) // => "{ term: hey" }/
-
-  res.render('home')
+// Handle the home page when we haven't queried yet
+term = ""
+if (req.query.term) {
+    term = req.query.term
+}
+// Tenor.search.Query("SEARCH KEYWORD HERE", "LIMIT HERE")
+Tenor.Search.Query(term, "10")
+    .then(response => {
+        // store the gifs we get back from the search
+        const gifs = response;
+        // pass the gifs as an object into the home page
+        res.render('home', { gifs })
+    }).catch(console.error);
 })
 
 app.get('/greetings/:name', (req, res) => {
